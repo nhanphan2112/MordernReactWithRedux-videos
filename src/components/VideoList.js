b77af0910625ad7584978e58bf81e4304891a856
Nhan Phan <nphanacc@gmail.com>
2021-06-14 12:15:44 -0700

    128-Rendering Video Thumbnails and titles

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,8 +3,9 @@ import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos }) => {
 	// props.videos from App.js were desconstructed into videos at line 4
+  // console.log(videos);
 	const renderList = videos.map((video) => {
-		return <VideoItem />;
+		return <VideoItem video={video}/>;
 	});
 	return <div>{renderList}</div>;
 };
