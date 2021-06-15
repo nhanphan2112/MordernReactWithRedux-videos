f1d300cd2e83cfec40d3a63c84db88fb8b92b2c0
Nhan Phan <nphanacc@gmail.com>
2021-06-15 09:16:30 -0700

    131-Deeply Nested Callbacks

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
 	// props.videos from App.js were desconstructed into videos at line 4
   // console.log(videos);
 	const renderList = videos.map((video) => {
-		return <VideoItem video={video}/>;
+		return <VideoItem onVideoSelect={onVideoSelect} video={video}/>;
 	});
 	return <div className="ui relaxed divided list">{renderList}</div>;
 };
