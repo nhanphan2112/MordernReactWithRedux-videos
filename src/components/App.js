7c78ff29b4eff80f57516ed2844a9611997c57bc
Nhan Phan <nphanacc@gmail.com>
2021-06-15 10:25:46 -0700

    136-make sure loading video show some defaults videos and VideoDetail component updated on each search

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,10 @@ import VideoDetail from "./VideoDetail";
 class App extends React.Component {
 	state = { videos: [], selectedVideo: null };
 
+	componentDidMount() {
+    this.onTermSubmit('hello kitty');
+  }
+
 	onTermSubmit = async (term) => {
 		const response = await youtube.get("/search", {
 			params: {
@@ -14,7 +18,10 @@ class App extends React.Component {
 			},
 		});
 
-		this.setState({ videos: response.data.items });
+		this.setState({
+			videos: response.data.items,
+			selectedVideo: response.data.items[0],
+		});
 		// console.log(response);
 		// console.log(this.state.videos);
 	};
@@ -30,8 +37,12 @@ class App extends React.Component {
 				<SearchBar onFormSubmit={this.onTermSubmit} />
 				<div className="ui grid">
 					<div className="ui row">
-						<div className="eleven wide column"><VideoDetail video={this.state.selectedVideo} /></div>
-						<div className="five wide column"><VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} /></div>
+						<div className="eleven wide column">
+							<VideoDetail video={this.state.selectedVideo} />
+						</div>
+						<div className="five wide column">
+							<VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+						</div>
 					</div>
 				</div>
 			</div>
